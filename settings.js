5f12ebace1312b22b3a104fb70bfbdc1dbff8361
FedelanYT <fedelanyt20@gmail.com>
2025-06-07 20:15:45 -0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,92 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+global.botNumber = '' //Ejemplo: 543218138672
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = [
+  ['5491156178758', '🜲 Propietario 🜲', true],
+  ['573015270038', 'staff', true],
+];
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = []
+global.suittag = ['5491156178758', '573015270038'] 
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.libreria = 'Baileys'
+global.baileys = 'V 6.7.16' 
+global.vs = '2.2.0'
+global.nameqr = 'Asuna-Bot'
+global.namebot = '✿ ᥲsᥙᥒᥲᑲ᥆𝗍-ᥲᥣ ✿'
+global.sessions = 'Sessions'
+global.jadi = 'JadiBots' 
+global.yukiJadibts = true
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = 'ᥲsᥙᥒᥲᑲ᥆𝗍-ᥲᥣ'
+global.botname = '❀ ᥲsᥙᥒᥲᑲ᥆𝗍-ᥲᥣ ❀'
+global.wm = '↬ ძᥱ᥎: ᑲᥡ 𝖿ᥱძᥱ'
+global.author = 'ძᥱ᥎ 𝖿ᥱძᥱᥣᥲᥒᥡ𝗍'
+global.dev = 'ძᥱ᥎ ᥲsᥙᥒᥲ_ᑲ᥆𝗍 | ᥴrᥱᥲ𝗍᥆r  𝖿ᥱძᥱᥣᥲᥒᥡ𝗍'
+global.textbot = '✧ ძᥱ᥎ᥱᥣ᥆⍴ᥱძ ᑲᥡ 𝖿ᥱძᥱᥣᥲᥒᥡ𝗍 ✧'
+global.etiqueta = '✦ 𝖿ᥱძᥱᥣᥲᥒᥡ𝗍'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.moneda = '✿ 𝖢𝗁𝗈𝖼𝗈𝗅𝖺𝗍𝖾𝗌 ✿'
+global.welcom1 = '❍ Edita Con El Comando setwelcome'
+global.welcom2 = '❍ Edita Con El Comando setbye'
+global.banner = 'https://qu.ax/FLKXQ.jpg'
+global.avatar = 'https://files.catbox.moe/iigsua.jpg'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.gp1 = 'https://chat.whatsapp.com/CnHfmUxlC8V7aGl7uC8LvO'
+global.comunidad1 = 'https://chat.whatsapp.com/FX6eYrqXtt9L76NDpOm2K7'
+global.channel = 'https://whatsapp.com/channel/0029VbApe6jG8l5Nv43dsC2N'
+global.channel2 = 'https://whatsapp.com/channel/0029VbApe6jG8l5Nv43dsC2N'
+global.md = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot'
+global.correo = 'thekingdestroy507@gmail.com'
+global.cn ='https://whatsapp.com/channel/0029VbApe6jG8l5Nv43dsC2N';
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.catalogo = fs.readFileSync('./src/catalogo.jpg');
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.ch = {
+ch1: '120363416409380841@newsletter',
+}
+global.multiplier = 70
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+global.moment = moment   
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'settings.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
