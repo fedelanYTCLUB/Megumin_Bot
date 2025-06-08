ecfde24d66f23d889285ab548a5fe4224d13b313
FedelanYT <fedelanyt20@gmail.com>
2025-06-08 13:58:50 -0300

    Create main-menu+18.js

---
+++ plugins/main-menu+18.js
@@ -0,0 +1,45 @@
+let handler = async (m, { isPrems, conn }) => {
+
+let img = 'https://files.catbox.moe/16bm7g.jpg' 
+let texto = `• 𝐌𝐄𝐍𝐔 +18 
+
+*꒰ 🔥 ꒱* #anal + <mencion
+*꒰ 🔥 ꒱* #waifu
+*꒰ 🔥 ꒱* #bath + <mencion
+» Bañarse
+*꒰ 🔥 ꒱* #blowjob • #mamada • #bj + <mencion
+*꒰ 🔥 ꒱* #boobjob + <mencion
+*꒰ 🔥 ꒱* #cum + <mencion
+*꒰ 🔥 ꒱* #fap + <mencion
+*꒰ 🔥 ꒱* #ppcouple • #ppcp
+*꒰ 🔥 ꒱* #footjob + <mencion
+*꒰ 🔥 ꒱* #fuck • #coger • #fuck2 + <mencion
+*꒰ 🔥 ꒱* #cafe • #coffe
+*꒰ 🔥 ꒱* #violar • #perra + <mencion
+*꒰ 🔥 ꒱* #grabboobs + <mencion
+*꒰ 🔥 ꒱* #grop + <mencion
+*꒰ 🔥 ꒱* #lickpussy + <mencion
+*꒰ 🔥 ꒱* #rule34 • #r34 + [Tags]
+*꒰ 🔥 ꒱* #sixnine • #69 + <mencion
+*꒰ 🔥 ꒱* #spank • #nalgada + <mencion
+*꒰ 🔥 ꒱* #suckboobs + <mencion
+*꒰ 🔥 ꒱* #undress • #encuerar + <mencion
+*꒰ 🔥 ꒱* #yuri • #tijeras + <mencion
+
+ conn.sendMessage(m.chat, {
+        text: texto,
+        contextInfo: {
+        externalAdReply: {
+        title: '𓂂𓏸  𐅹੭੭ ᴍᴇɴᴜ ᴅᴇ ᴘᴏʀɴᴏɢʀᴀғɪᴀ 🌙 ᦡᦡ',
+        body: dev,
+        thumbnailUrl: img,
+        sourceUrl: channel,
+        mediaType: 1,
+        renderLargerThumbnail: true
+        }}},
+        { quoted: fkontak })
+}
+handler.help = ['menuhot (menu +18)']
+handler.tags = ['Crow']
+handler.command = ['menu18', 'menunsfw', 'menuhot'] 
+export default handler;
